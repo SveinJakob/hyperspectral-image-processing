fd4a64cb48c04d2e41a810680d5b46996729953f
Svein Jakob Kristoffersen <sveinjakob1@hotmail.com>
2022-01-19 20:05:01 +0100

    Folder for example scripts, preprocess_image.py started

---
+++ Code Examples/preprocess_image.py
@@ -0,0 +1,8 @@
+image_info = {}
+
+
+def preprocess_image(_image_info):
+    path_img = _image_info['Path image']
+    path_hdr = _image_info['Path hdr']
+    ref_area = _image_info['Area w.r.']
+    leaf_area = _image_info['Area leaf']
